99105b123dab77b4dd756fb23ac6bb780fbadc50
Cra2yPierr0t <Cra2yPierr0t@vlsi.jp>
2023-05-29 22:59:46 +0900

    improve behavior model

--- beh/KVMemory_beh.sv
+++ beh/KVMemory_beh.sv
@@ -28,7 +28,56 @@ module KVMemory_beh #(
   logic [3:0] w_shift_reg;
   logic [3:0] r_shift_reg = 4'b0000;
 
+  logic [2:0] w_cnt;
+  logic [2:0] r_cnt = '0;
+
   // Read
+  always_comb begin
+    if(~i_read_valid) begin
+      o_read_ready = i_read_ready;
+    end else if(r_cnt == 3'b100) begin
+      o_read_ready = i_read_ready;
+    end else begin
+      o_read_ready  = i_read_ready & o_read_valid;
+    end
+    if((r_cnt == 3'b011)) begin
+      w_o_read_valid    = i_read_valid;
+      w_o_read_data[0]  = r_mem[{i_read_addr[2], 2'b00}];
+      w_o_read_data[1]  = r_mem[{i_read_addr[2], 2'b01}];
+      w_o_read_data[2]  = r_mem[{i_read_addr[2], 2'b10}];
+      w_o_read_data[3]  = r_mem[{i_read_addr[2], 2'b11}];
+    end else begin
+      w_o_read_valid    = 1'b0;
+      w_o_read_data[0]  = o_read_data[0];
+      w_o_read_data[1]  = o_read_data[1];
+      w_o_read_data[2]  = o_read_data[2];
+      w_o_read_data[3]  = o_read_data[3];
+    end
+    if(i_read_ready) begin
+      if(i_read_valid) begin
+        if(r_cnt == 3'b100) begin
+          w_cnt = '0;
+        end else begin
+          w_cnt = r_cnt + 3'b001;
+        end
+      end else begin
+        w_cnt = '0;
+      end
+    end else begin
+      w_cnt = r_cnt;
+    end
+  end
+
+  always_ff @(posedge i_clk) begin
+    o_read_data[0]  <= w_o_read_data[0];
+    o_read_data[1]  <= w_o_read_data[1];
+    o_read_data[2]  <= w_o_read_data[2];
+    o_read_data[3]  <= w_o_read_data[3];
+    o_read_valid    <=  w_o_read_valid;
+    r_cnt           <=  w_cnt;
+  end
+
+  /*
   always_comb begin
     o_read_valid = r_shift_reg[3];
     if(~r_shift_reg[3] || i_read_ready) begin
@@ -59,5 +108,6 @@ module KVMemory_beh #(
     o_read_data[2]  <= w_o_read_data[2];
     o_read_data[3]  <= w_o_read_data[3];
   end
+  */
 
 endmodule
