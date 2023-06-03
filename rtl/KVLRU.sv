55135ed6d8caa75a60ae3cf264284731fe442417
Cra2yPierr0t <Cra2yPierr0t@vlsi.jp>
2023-06-04 05:00:21 +0900

    LRU maybe works

--- rtl/KVLRU.sv
+++ rtl/KVLRU.sv
@@ -6,16 +6,25 @@ module KVLRU #(
 )(
   input  logic  i_clk,
   input  logic  i_rstn,
-  input  logic  [WAY_NUM-1:0]       i_valid_way,
   input  logic  [WAY_NUM-1:0]       i_hitway,
   input  logic  [INDEX_WIDTH-1:0]   i_index,
   output logic  [WAY_NUM-1:0]       o_killmask
 );
 
+  logic [1:0] debug_bit2index;
+  logic [1:0] debug_lru_array[3:0];
+
   logic [WAY_NUM-1:0] w_killmask;
+  logic [WAY_NUM-1:0] r_killmask;
+  logic [WAY_NUM-1:0] w_uso_killmask;
+  logic [WAY_NUM-1:0] r_uso_killmask;
 
   logic [1:0] w_lru_array[3:0];
-  logic [1:0] r_lru_array[3:0][LINE_NUM_PER_WAY-1:0];
+  logic [1:0] r_lru_array[LINE_NUM_PER_WAY][3:0];
+
+  logic w_miss;
+  logic [WAY_NUM-1:0] w_all_zero_check;
+  logic w_all_zero;
 
   function [$clog2(WAY_NUM)-1:0] bit2index(input [WAY_NUM-1:0] bits);
     bit [$clog2(WAY_NUM):0] way;
@@ -27,45 +36,86 @@ module KVLRU #(
     return ($clog2(WAY_NUM))'(WAY_NUM-1);
   endfunction
 
-  always_comb begin
-    if(i_hitway = 4'b0000) begin
-      w_killmask = 4'b0001;
-    end else begin
-
+  generate for(genvar way = 0; way < WAY_NUM; way = way + 1) begin
+    always_comb begin
+      w_uso_killmask[way]   = (r_lru_array[i_index][way] == '1);
     end
+  end endgenerate
+
+  always_ff @(posedge i_clk) begin
+    r_uso_killmask  <= w_uso_killmask;
   end
 
-  generate
-    for(genvar way = 0; way < WAY_NUM; way = way + 1) begin
-      always_comb begin
-        if(r_lru_array[i_index][way] == 2'b11) begin
-          w_killmask[way]   = 1'b1;
-        end else begin
-          w_killmask[way]   = 1'b0;
-        end
+  generate for(genvar way = 0; way < WAY_NUM; way = way + 1) begin
+    always_comb begin
+      if(r_lru_array[i_index][way] == '0) begin
+        w_all_zero_check[way]   = 1'b1;
+      end else begin
+        w_all_zero_check[way]   = 1'b0;
       end
     end
-  endgenerate
+  end endgenerate
 
-  generate
-    for(genvar way = 0; way < WAY_NUM; way = way + 1) begin
-      always_comb begin
-        if(i_hitway == 4'b0000) begin
+  always_comb begin
+    w_miss      = ~|i_hitway;
+    w_all_zero  = &w_all_zero_check;
+  end
+
+  generate for(genvar way = 0; way < WAY_NUM; way = way + 1) begin
+    always_comb begin
+      if(~|r_uso_killmask) begin    // all uso killmask is zero
+        if(way == 0) begin
+          if(w_all_zero) begin
+            w_killmask[way]   = 1'b1;
+            w_lru_array[way]    = 0;
+          end else begin
+            w_killmask[way]   = 1'b0;
+            w_lru_array[way]    = r_lru_array[i_index][way] + 1;
+          end
+        end else begin
+          if(w_all_zero) begin
+            w_killmask[way]   = 1'b0;
+            w_lru_array[way]    = 1;
+          end else begin
+            if(r_lru_array[i_index][way-1] == 0) begin
+              w_killmask[way]   = 1'b1;
+              w_lru_array[way]  = '0;
+            end else begin
+              w_killmask[way]   = 1'b0;
+              w_lru_array[way]  = r_lru_array[i_index][way] + 1;
+            end
+          end
+        end
+      end else begin
+        w_killmask[way] = (r_lru_array[i_index][way] == '1);
+        if(w_miss) begin
+          if(bit2index(w_killmask) == way) begin
+            w_lru_array[way]  = '0;
+          end else begin
+            w_lru_array[way]  = r_lru_array[i_index][way] + 1;
+          end
         end else begin
           if(i_hitway[way]) begin
-            w_lru_array[way] = 2'b00;
+            w_lru_array[way]  = '0;
           end else begin
-            w_lru_array[way] = r_lru_array[i_index][way] + 2'b01;
+            if(r_lru_array[i_index][way] < r_lru_array[i_index][bit2index(i_hitway)]) begin
+              w_lru_array[way]    = r_lru_array[i_index][way] + 1;
+            end else begin
+              w_lru_array[way]    = r_lru_array[i_index][way];
+            end
           end
         end
       end
     end
-  endgenerate
+  end endgenerate
 
   always_ff @(posedge i_clk) begin
     r_lru_array[i_index]    <= w_lru_array;
-    o_killmask              <= w_killmask;
+    r_killmask              <= w_killmask;
   end
 
+  always_comb begin
+    o_killmask  = w_killmask;
+  end
 
 endmodule
--- tb/KVLRU_tb.sv
+++ tb/KVLRU_tb.sv
@@ -1,7 +1,6 @@
 module KVSelectInvalidWay_tb;
 
-  logic         i_clk;
-  logic [3:0]   i_valid_way;
+  logic         i_clk = '1;
   logic [3:0]   i_hitway;
   logic [3:0]   i_index;
   logic [3:0]   o_killmask;
@@ -9,11 +8,14 @@ module KVSelectInvalidWay_tb;
   KVLRU DUT (
     .i_clk          (i_clk          ),
     .i_rstn         (),
-    .i_valid_way    (i_valid_way    ),
     .i_hitway       (i_hitway       ),
     .i_index        (i_index        ),
     .o_killmask     (o_killmask     )
   );
+
+  always #1 begin
+    i_clk   <= ~i_clk;
+  end
 
   initial begin
     $dumpfile("wave.vcd");
@@ -21,11 +23,7 @@ module KVSelectInvalidWay_tb;
   end
 
   initial begin
-    #2
-    i_valid_way = 4'b0001;
-    #2
-    #2
-    #2
+    #20
     $finish;
   end
 endmodule
